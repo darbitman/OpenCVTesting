d01edbb053460d7157c2233f9bfc18e8339e9ef6
Dmitriy <d.arbitman@gmail.com>
2018-06-22 20:48:13 -0700

    Working on image filter

--- PlayVideo/PlayVideo.cpp
+++ PlayVideo/PlayVideo.cpp
@@ -5,6 +5,47 @@ using namespace std;
 using namespace cv;
 
 int main() {
+  // open video file for reading
+  VideoCapture vid("../PlayVideo/nature.mp4");
+
+  // check if video file opened
+  if (vid.isOpened() == false) {
+    cout << "Cannot open the video file" << endl;
+    // wait for keypress before continuing
+    cin.get();
+    return -1;
+  }
+
+  // get framerate
+  double fps = vid.get(CAP_PROP_FPS);
+
+  String windowName = "First Video";
+
+  // create window for playing video
+  namedWindow(windowName, WINDOW_NORMAL);
+
+  while (true) {
+    Mat frame;
+
+    // get single frame from video
+    bool success = vid.read(frame);
+
+    if (success == false) {
+      cout << "End of video" << endl;
+      break;
+    }
+
+    // show single frame in window
+    imshow(windowName, frame);
+
+    // wait for 10ms until a key is pressed
+    // if 'Esc' is pressed, stop the video
+    // otherwise continue playback
+    if (waitKey(10) == 27) {
+      cout << "Esc key pressed. Stopping the video..." << endl;
+      break;
+    }
+  }
 
   return 0;
 }
